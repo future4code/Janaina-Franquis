b9937f7f47618f707896e1a33f8430190cdd6541
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-04-27 15:32:18 -0300

    Exercicio aula-15

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -1,8 +1,7 @@
-function createTask (){
-    let selection = document.getElementById("dias-semana").value
-    let task = document.getElementById("tarefa").value
-    document.getElementById(selection).innerHTML += "<p>- " + task + "</p>"
-    document.getElementById("tarefa").value = ""
-    document.getElementById("dias-semana").value = "domingo"
-
+function createTask() {
+  let selection = document.getElementById("dias-semana").value;
+  let task = document.getElementById("tarefa").value;
+  document.getElementById(selection).innerHTML += "<p>- " + task + "</p>";
+  document.getElementById("tarefa").value = "";
+  document.getElementById("dias-semana").value = "domingo";
 }
