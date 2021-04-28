4cff804ed4f34df205585e22a22a4b486143a92b
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-04-28 14:30:40 -0300

    adicionando enunciado para identificar o que foi feito

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -1,6 +1,9 @@
+/* 1 - Crie uma função que é chamada ao clicar no botão "Criar Tarefa". 
+       Use um evento apropriado que identifique o clique no botão.*/
+
 function createTask() {
-  let selection = document.getElementById("dias-semana").value;
-  let task = document.getElementById("tarefa").value;
+  let selection = document.getElementById("dias-semana").value; /* 3 - Capture o valor do seletor do HTML (o dia da semana selecionado) e armazene em uma variáve*/
+  let task = document.getElementById("tarefa").value;/*2- Dentro da função, capture o valor do campo do HTML (que é a tarefa a ser colocada no planner) e armazene em uma variável.*/
   document.getElementById(selection).innerHTML += "<p>- " + task + "</p>";
   document.getElementById("tarefa").value = "";
   document.getElementById("dias-semana").value = "domingo";
