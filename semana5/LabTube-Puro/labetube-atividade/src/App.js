d1fec91c17ec66a35a9e898ab4d5b6543b0e4498
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-06-04 15:56:21 -0300

    revisao labTube

--- semana5/LabTube-Puro/labetube-atividade/src/App.js
+++ semana5/LabTube-Puro/labetube-atividade/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import CardVideo from './components/CardVideo';
+import BotaoMenu from "./components/BotaoMenu/BotaoMenu";
+import styled from "styled-components";
 
+const MeuFooter = styled.footer`
+  height: 10%;
+  background-color: gold;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+
+`;
+ 
 function App() {
-  const titulo = "Titulo do vídeo"
+  
 
   function reproduzVideo() {
     alert("O vídeo está sendo reproduzido")
@@ -10,71 +23,46 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Lab Tube</h1>
+        <h1>LabTube</h1>
         <input type="text" placeholder="Busca" id="campoDeBusca" />
       </header>
 
       <main>
         <nav className="menu-vertical">
           <ul>
-            <li className="botoes-menu-vertical">Início</li>
-            <li className="botoes-menu-vertical">Em alta</li>
-            <li className="botoes-menu-vertical">Inscrições</li>
+            <BotaoMenu textoDoItem={"Ínicio"}/> 
+            <BotaoMenu textoDoItem={"Em Alta"}/>
+            <BotaoMenu textoDoItem={"Inscrições"}/>
             <hr />
-            <li className="botoes-menu-vertical">Originais</li>
-            <li className="botoes-menu-vertical">Histórico</li>
+            <BotaoMenu textoDoItem={"Biblioteca"}/>
+            <BotaoMenu textoDoItem={"Histórico"}/>
           </ul>
         </nav>
 
         <section className="painel-de-videos">
-          <div className="box-pagina-principal media1" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=1 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
-          <div className="box-pagina-principal media2" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=2 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
-          <div className="box-pagina-principal media3" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=3 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
-          <div className="box-pagina-principal media4" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=4 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
-          <div className="box-pagina-principal media5" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=5 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
-          <div className="box-pagina-principal media6" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=6 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
-          <div className="box-pagina-principal media7" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=7 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
-          <div className="box-pagina-principal media8" onClick={reproduzVideo
-          }>
-            <img src="https://picsum.photos/400/400?a=8 " alt="" />
-            <h4>{titulo}</h4>
-          </div>
+          <CardVideo tituloDoVideo={"Titulo 1"} imagem={"https://picsum.photos/400/400?a=1"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 2"} imagem={"https://picsum.photos/400/400?a=2"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 3"} imagem={"https://picsum.photos/400/400?a=3"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 4"} imagem={"https://picsum.photos/400/400?a=4"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 5"} imagem={"https://picsum.photos/400/400?a=5"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 6"} imagem={"https://picsum.photos/400/400?a=6"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 7"} imagem={"https://picsum.photos/400/400?a=7"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 8"} imagem={"https://picsum.photos/400/400?a=8"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 9"} imagem={"https://picsum.photos/400/400?a=8"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 10"} imagem={"https://picsum.photos/400/400?a=9"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 11"} imagem={"https://picsum.photos/400/400?a=10"} funcaoClick={reproduzVideo}/>
+          <CardVideo tituloDoVideo={"Titulo 12"} imagem={"https://picsum.photos/400/400?a=11"} funcaoClick={reproduzVideo}/>
         </section>
       </main>
 
-      <footer>
+      <MeuFooter>
         <h4>Oi! Eu moro no footer!</h4>
-      </footer>
+      </MeuFooter>
     </div>
   );
 }
 
 export default App;
+
+// export function MeuComponente => import {MeuComponente}
+// export default function MeuComponente = import MeuComponente
--- semana5/LabTube-Puro/labetube-atividade/src/components/cardVideo.js
+++ semana5/LabTube-Puro/labetube-atividade/src/components/cardVideo.js
@@ -1,7 +1,37 @@
-import React from "react"
+import React from "react";
 
-export function CardVideo(){
+
+
+// utilizando props para importar imagem, titulo do video e função do botão
+/*
+export default function CardVideo (props){
+    console.log(props)
     return(
+        <div 
+           className="box-pagina-principal media1" 
+           onClick={() => props.funcaoClick(props.tituloDoVideo)}>
+          <img src={props.imagem} alt="" />
+          <h4>{props.tituloDoVideo}</h4>
+        </div>
         
     )
+}
+*/
+//Função sem parametro: onClick={props.funcaoClick}>
+// Função com parametro:() => props.funcaoClick("Olá")
+
+// Transformando em componente de classe
+export default class CardVideo extends React.Component {
+      render () {
+        return (
+            <div 
+                className="box-pagina-principal media1"
+                onClick={() => this.props.funcaoClick(this.props.tituloDoVideo)}
+                >
+                <img src={this.props.imagem} alt="" />
+                <h4>{this.props.tituloDoVideo}</h4>
+            </div>
+        )
+    }
+
 }
