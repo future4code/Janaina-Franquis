4bb26a47de77c548311beac1b1ef6fe6e30e5753
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-06-04 16:00:53 -0300

    Revisao insta4

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -9,39 +9,31 @@ const MainContainer = styled.div`
   align-items: center;
 `;
 
-class App extends React.Component {
-  render() {
-    const listaDeNomes = [<p>Maia Fernandes</p>, <p>Ricardo Teixeira</p>, <p>César Augusto</p>,];
-    
+export default class App extends React.Component {
+    render () {
     return (
-      <div>
-        <h1>{listaDeNomes}</h1>
-      </div>
+      <div className="app-container">
+                <Post
+                  nomeUsuario={"Janaina"}
+                  fotoUsuario={"https://picsum.photos/50/50"}
+                  fotoPost={"https://picsum.photos/200/150"}
+                />
+
+                <Post
+                  nomeUsuario={"José"}
+                  fotoUsuario={"https://picsum.photos/50/5"}
+                  fotoPost={"https://picsum.photos/200/150"}
+                />
+
+                <Post
+                  nomeUsuario={"Maria Eugênia"}
+                  fotoUsuario={"https://picsum.photos/50/50"}
+                  fotoPost={"https://picsum.photos/200/150"}
+                />
+           
+   </div>
+      
     );
   }
 }
 
-/* <MainContainer>
-        <Post
-          nomeUsuario={"Janaina"}
-          fotoUsuario={"https://picsum.photos/50/50"}
-          fotoPost={"https://picsum.photos/200/150"}
-        />
-
-        <Post
-          nomeUsuario={"José"}
-          fotoUsuario={"https://picsum.photos/50/5"}
-          fotoPost={"https://picsum.photos/200/150"}
-        />
-
-        <Post
-          nomeUsuario={"Maria Eugênia"}
-          fotoUsuario={"https://picsum.photos/50/50"}
-          fotoPost={"https://picsum.photos/200/150"}
-        />
-      </MainContainer>
-    );
-  }
-}*/
-
-export default App;
