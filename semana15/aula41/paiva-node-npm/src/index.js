438f1744f46d7ae14952594b97d01f3d1a3d2423
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-07-18 18:56:36 -0300

    forma de usar process.argv

--- semana15/aula41/paiva-node-npm/src/index.js
+++
@@ -1,3 +0,0 @@
-const num = Number(process.argv[2])
-
-console.log(`O dobro do seu número é ${2 * num}`)
--- semana15/aula41/template-node-npm/index.js
+++ semana15/aula41/template-node-npm/index.js
@@ -1,2 +1,8 @@
 const countries = require("./countries")
-console.log(countries)
+//const num = process.argv[2]
+const num = Number(process.argv[2]) // soma o nº 2 + nº 7 informado no terminal = 9. Neste caso roda a operação matemática
+
+ console.log(`O dobro do seu nº é ${2 + num}`)
+//concatena o nº 2 + nº 9 informado no terminal = 29 
+//console.log(`O dobro do seu nº é ${2 * num}`) multiplica o nº 2 + o nº 5 informado no terminal = 10
+//rodar no terminal node index.js 9
