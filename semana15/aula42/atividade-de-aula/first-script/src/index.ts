87ae215513e790334a4740f2c57cfff22334b9bf
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-07-18 23:14:06 -0300

    Atividade da aula42

---
+++ semana15/aula42/atividade-de-aula/first-script/src/index.ts
@@ -0,0 +1,42 @@
+let userName: string = "Janaina"
+let age : number | string = "38" // aceitar idade com nº e string 
+//age = 8
+let estuda : boolean = true
+
+//console.log(typeof age) // string
+
+let messages: string [] = [] // array vazio
+let nomes : string [] = ["Janaina", "Ana", "Donna"] 
+let numerosComponentes : number[] = [1,2,3,4,5,8]
+var numerosDePessoas : (number|string) [] = [1, 2 , "3", "4"] // misturar nº e string com array
+
+//console.log(nomes)
+//console.log(numerosComponentes)
+
+console.log(numerosDePessoas)
+
+// esquelo que define as propriedades
+type person = {
+    name: string,
+    age: number,
+    address: string
+}
+
+const astrodev : person = {
+    name: "Astrodev",
+    age: 35,
+    address: "Rua de Vênus",
+
+}
+
+console.log(astrodev)
+
+// tipar funções
+
+function sum (
+    a: number, 
+    b: number
+    ) : number {
+        return a + b
+    }
+//1:30:08
