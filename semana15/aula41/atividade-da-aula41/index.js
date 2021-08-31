99b6da2b4a9042ab2dea9c97d3f218adb7e5b580
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-08-30 21:15:16 -0300

    Exercicios  aula-41 do 1 ao 3

--- semana15/aula41/atividade-da-aula41/index.js
+++ semana15/aula41/atividade-da-aula41/index.js
@@ -1,5 +1,35 @@
-const nameAge = (name, age) => {
-    return console.log (
-        `Ola ${name}! Você tem ${age} anos.`
-    )
-}
+// //A. acessados no código pela propriedade process.argv[2] e chamar a variavel atribuida no console.log .
+
+// //B.
+// const nome = "Janaina";
+// const age = 39;
+// console.log(`Olá  ${nome}!  Você tem  ${age} anos.`);
+
+// //C.
+// const number = Number("7")
+// console.log(`Olá  ${nome}!  Você tem  ${age} anos. Em sete anos você terá   ${number+age}`);
+
+//EXERCÍCIO 2
+// const operacao = (process.argv[2]);
+// const number1 = Number(process.argv[3])
+
+// switch (operacao) {
+//     case "add":
+//         return console.log(`Resposta: ${number1 + 2}`)
+//     case "sub":
+//         return console.log(`Resposta: ${number1 - 2}`)
+//     case "mult":
+//         return console.log(`Resposta: ${number1 * 2}`)
+//     case "div":
+//         return console.log(`Resposta: ${number1 / 2}`)
+//     default:
+//         break;
+// }
+
+//EXERCÍCIO 3
+const listaDeTarefas = ["Ir ao banco", "Lavar Roupa, Lavar louça, Comprar leite"]
+console.log("Tarefa adicionada com sucesso!")
+listaDeTarefas.push(process.argv[2])
+console.log(listaDeTarefas)
+
+
---
+++ semana15/aula41/template-node-npm/index.js
@@ -0,0 +1,3 @@
+
+const num = Number(process.argv[2])
+console.log(`O dobro do seu numero é ${5 + num}`)
